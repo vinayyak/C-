b2252496fe94c9a31511eec90419022061163623
bigvin8 <60383210+bigvin8@users.noreply.github.com>
2020-03-29 23:36:05 +0530

    Create NEGPOS.c

---
+++ NEGPOS.c
@@ -0,0 +1,21 @@
+#include <stdio.h> 
+#include<conio.h> 
+ void main()  
+{  
+    int n;    
+printf("enter a number:");    
+scanf("%d",&n);     
+if(n>0)
+{    
+printf("the number is positive");    
+}    
+else if(n<0)
+{    
+printf("the number is negative");    
+}    
+else
+{    
+printf("the number is 0");    
+}     
+    getch();
+    }
