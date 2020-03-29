79b4451666f90a94fae616373287513836af540f
bigvin8 <60383210+bigvin8@users.noreply.github.com>
2020-03-29 23:29:52 +0530

    Create FACTORIAL.c

---
+++ FACTORIAL.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int n,f=1,i;
+printf("Enter the number");
+scanf("%d",&n);
+for(i=1;i<=n;i++)
+f=f*i;
+printf("The factorial is %d",f);
+getch();
+}
+© 2020 GitHub, Inc.
