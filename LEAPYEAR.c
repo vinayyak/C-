6582c562dbd31b21c5fad3a2bba6d78cf11b1c56
bigvin8 <60383210+bigvin8@users.noreply.github.com>
2020-03-29 23:33:45 +0530

    Create LEAPYEAR.c

---
+++ LEAPYEAR.c
@@ -0,0 +1,17 @@
+#include <stdio.h> 
+#include<conio.h> 
+ void main()  
+{  
+    int n;    
+printf("enter the year");    
+scanf("%d",&n);     
+if(n%4==0)
+{    
+printf("it is a leap year");    
+}    
+else
+{    
+printf("it is not a leap year");    
+}     
+    getch();
+    }
